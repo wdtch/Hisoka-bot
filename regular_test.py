18be76e63d09f41ee5173678fce33d9b2ce48759
wdtch <rever.psif@live.jp>
2015-08-26 23:57:15 +0900

    Modified Typo

--- regular_test.py
+++ regular_test.py
@@ -5,7 +5,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 b_scheduler = BlockingScheduler()
 
 # 30分ごとに動かしたい(とりあえず確認しやすいように3分ごと)
-@b_scheduler.scheled_job("interval", minutes=3)
+@b_scheduler.scheduled_job("interval", minutes=3)
 def thirty_minutes_job():
     print("This job works every 3 minutes.")
 
