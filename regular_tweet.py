b6ce76dfdb24ef8493732c17cf123075e30d268f
wdtch <rever.psif@live.jp>
2015-08-27 13:10:41 +0900

    Modified and try first

--- regular_tweet.py
+++ regular_tweet.py
@@ -6,7 +6,6 @@ import random
 import auth
 
 b_scheduler = BlockingScheduler()
-oauth = auth.Auth()
 
 @b_scheduler.scheduled_job("interval", minutes=1)
 def regular_tweet():
@@ -17,13 +16,13 @@ def regular_tweet():
 
     # OAuth認証でPOST methodを用いてツイートを投稿
     tweet_url = "https://api.twitter.com/1.1/statuses/update.json"
-    req = oauth.twitter.post(tweet_url, params=params)
+    req = auth.twitter.post(tweet_url, params=params)
 
     # レスポンスを確認
     if req.status_code == 200:
         print("Tweet Succeeded.")
     else:
-        print("Status Code {0}" % req.status_code)
+        print("Error: Status Code %d" % req.status_code)
 
 if __name__ == '__main__':
     b_scheduler.start()
