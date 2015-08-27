bb46045c805b6b5d5ba7790f04d6c9fe74ee1ea4
wdtch <rever.psif@live.jp>
2015-08-27 14:54:09 +0900

    Avoid duplicate error

--- regular_tweet.py
+++ regular_tweet.py
@@ -7,12 +7,12 @@ import random
 import auth
 
 b_scheduler = BlockingScheduler()
+f = open("random_tweet.txt")
+tweets = f.readlines().copy()
 
 # 30分毎に定期ツイート
 @b_scheduler.scheduled_job("interval", minutes=1)
 def regular_tweet():
-    f = open("random_tweet.txt")
-    tweets = f.readlines()
     tweet = random.choice(tweets)
     params = {"status": tweet}
 
@@ -24,7 +24,21 @@ def regular_tweet():
     if req.status_code == 200:
         print("Tweet Succeeded.")
     elif req.status_code == 403: # 重複？したとき
-        regular_tweet() # リトライ
+        tweet_again() # リトライ
+    else:
+        print("Error: Status Code {0}".format(req.status_code))
+
+def tweet_again():
+    tweet = random.choice(tweets)
+    params = {"status": tweet}
+
+    # OAuth認証でPOST methodを用いてツイートを投稿
+    tweet_url = "https://api.twitter.com/1.1/statuses/update.json"
+    req = auth.twitter.post(tweet_url, params=params)
+
+    # レスポンスを確認
+    if req.status_code == 200:
+        print("Tweet Succeeded.")
     else:
         print("Error: Status Code {0}".format(req.status_code))
 
