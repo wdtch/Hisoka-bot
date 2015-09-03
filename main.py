d2c617fa0dfadc570641bbc6943efbaee58a7980
wdtch <rever.psif@live.jp>
2015-09-04 01:03:04 +0900

    Changed order of scheduled function

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ b_scheduler = BlockingScheduler()
 reg_man = regular_tweet.RegularTweet()
 reply_man = reply.AutoReply()
 
-@b_scheduler.scheduled_job("interval", minutes=30)
-def run_regular_tweet():
-    reg_man.regular_tweet()
-
 @b_scheduler.scheduled_job("interval", minutes=1)
 def run_reply():
     reply_man.get_reply()
+
+@b_scheduler.scheduled_job("interval", minutes=30)
+def run_regular_tweet():
+    reg_man.regular_tweet()
 
 if __name__ == '__main__':
     b_scheduler.start()
