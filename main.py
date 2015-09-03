218e3ead253f5c5644cafc8aed81a58d9dca512d
wdtch <rever.psif@live.jp>
2015-09-03 16:12:13 +0900

    Scheduled Executor

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+import regular_tweet
+import reply
+
+b_scheduler = BlockingScheduler()
+reg_man = regular_tweet.RegularTweet()
+reply_man = reply.AutoReply()
+
+@b_scheduler.scheduled_job("interval", minutes=2)
+def run_regular_tweet():
+    reg_man.regular_tweet()
+
+@b_scheduler.scheduled_job("interval", minutes=1)
+def run_reply():
+    reply_man.get_reply()
+
+if __name__ == '__main__':
+    b_scheduler.start()
