568cf5b6c7effd3aec670ee3d664bddb4446b723
wdtch <rever.psif@live.jp>
2015-09-04 12:04:35 +0900

    Changed job writing

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ b_scheduler = BlockingScheduler()
 reg_man = regular_tweet.RegularTweet()
 reply_man = reply.AutoReply()
 
-@b_scheduler.scheduled_job("interval", minutes=3)
+# @b_scheduler.scheduled_job("interval", minutes=3)
 def run_reply():
     reply_man.get_reply()
 
-@b_scheduler.scheduled_job("interval", minutes=30)
+# @b_scheduler.scheduled_job("interval", minutes=30)
 def run_regular_tweet():
     reg_man.regular_tweet()
 
 if __name__ == '__main__':
+    b_scheduler.add_job(run_reply, "interval", id="run_reply", minutes=1)
+    b_scheduler.add_job(run_regular_tweet, "interval", id="run_regular_tweet", minutes=30)
     b_scheduler.start()
