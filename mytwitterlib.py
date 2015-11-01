d8b0f9eb0ec1457fef61f285d1b883727911f31b
wdtch <rever.psif@live.jp>
2015-11-01 15:11:52 +0900

    Implemented minimum functions

--- mytwitterlib.py
+++ mytwitterlib.py
@@ -13,7 +13,7 @@ class MyTwitterLib(object):
         self._since_id_m = ""
 
     # TL取得
-    def get_timeline(self, num=None, since_id=None, recording_since_id=False):
+    def get_timeline(self, num=None, since_id=None, recording_since_id=True):
         url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
 
         if num is not None:
@@ -33,10 +33,10 @@ class MyTwitterLib(object):
         # 取得成功
         if req.status_code == 200:
             # json形式で取得したタイムラインをパース
-            timeline = json.loads(req.text)
+            timeline = list(map(Tweet, json.loads(req.text)))
 
-            if recording_since_id:
-                self._since_id_tl = timeline[0]["id_str"]
+            if timeline != [] and recording_since_id:
+                self._since_id_tl = timeline[0].tweet_id
 
             return timeline
 
@@ -46,7 +46,7 @@ class MyTwitterLib(object):
             return None
 
     # mention取得
-    def get_mentions(self, num=None, since_id=None, recording_since_id=False):
+    def get_mentions(self, num=None, since_id=None, recording_since_id=True):
         url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json"
 
         if num is not None:
@@ -66,10 +66,10 @@ class MyTwitterLib(object):
         # 取得成功
         if req.status_code == 200:
             # json形式で取得したタイムラインをパース
-            mentions = json.loads(req.text)
+            mentions = list(map(Tweet, json.loads(req.text)))
 
-            if recording_since_id:
-                self._since_id_m = mentions[0]["id_str"]
+            if mentions != [] and recording_since_id:
+                self._since_id_m = mentions[0].tweet_id
 
             return mentions
 
@@ -92,10 +92,22 @@ class MyTwitterLib(object):
             print("Error code {}: Failed to tweet.")
             return -1
 
+
+
+class Tweet(object):
+
+    def __init__(self, req):
+        self.text = req["text"]
+        self.time = req["created_at"]
+        self.user_name = req["user"]["name"]
+        self.user_id = req["user"]["screen_name"]
+        self.tweet_id = req["id_str"]
+        self.reply_id = req["in_reply_to_screen_name"]
+
     # リプライ
-    def reply(self, text, mention):
+    def reply(self, text):
         url = "https://api.twitter.com/1.1/statuses/update.json"
-        params = {"status": text, "in_reply_to_status_id": mention["id_str"]}
+        params = {"status": text, "in_reply_to_status_id": self.tweet_id}
 
         req = self.session.post(url, params=params)
 
@@ -103,5 +115,20 @@ class MyTwitterLib(object):
         if req.status_code == 200:
             return 0
         else:
-            print("Error code {}: Failed to tweet.")
+            print("Error code {}: Failed to reply.")
             return -1
+
+# test
+if __name__ == '__main__':
+    import auth
+    twitter = MyTwitterLib(auth.CK, auth.CS, auth.AT, auth.AS)
+
+    with open("since_id.txt", "r") as f:
+        since_id = f.readline()
+
+    for tl in twitter.get_timeline(10):
+        print(tl.__dict__)
+
+    for m in twitter.get_mentions(num=10, since_id=since_id):
+        print(m.__dict__)
+    twitter.tweet("ツイートテストだお")
