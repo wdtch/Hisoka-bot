f3a06b68bcffc35f56aaadaac07dc111e001bb22
wdtch <rever.psif@live.jp>
2015-12-03 12:27:07 +0900

    For Heroku

---
+++ auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+CK = "5EI60HpJL0MJ89qLE8YphHqZH"
+CS = "4wmMJriRfAvIWdVTJJZkiNlXJXgFGelaCK0v6WrJplmIWBTdcA"
+AT = "3350006131-QDlIBldOfToh3l7nsLx1gWOPs8p03RdUFOb6fka"
+AS = "izSOQoEfpJiAT8XwKopq3Ur3qjpE2nMa3rfB8OELPwTrx"
---
+++ ftpauth.py
@@ -0,0 +1,3 @@
+host = "reverpsif.web.fc2.com"
+user = "reverpsif"
+pwd = "AEo@9Feh&j"
